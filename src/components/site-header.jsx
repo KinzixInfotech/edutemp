d942009fdafd9bf70654eb46bf01f50f8542914c
KinzixInfotech <mansajami2020@gmail.com>
2025-08-28 14:34:50 +0530

    updated

--- src/components/site-header.jsx
+++ src/components/site-header.jsx
@@ -27,32 +27,8 @@ export function SiteHeader() {
                 />
                 <h1 className="text-base font-medium">Dashboard</h1>
                 <div className="ml-auto flex items-center gap-2">
-                    <div className="flex flex-col gap-3">
-                        <Popover open={open} onOpenChange={setOpen}>
-                            <PopoverTrigger asChild>
-                                <Button
-                                    variant="outline"
-                                    id="date"
-                                    className="w-48 justify-between font-normal"
-                                >
-                                    {date ? date.toLocaleDateString() : "Select date"}
-                                    <ChevronDownIcon />
-                                </Button>
-                            </PopoverTrigger>
-                            <PopoverContent className="w-auto overflow-hidden p-0" align="start">
-                                <Calendar
-                                    mode="single"
-                                    selected={date}
-                                    captionLayout="dropdown"
-                                    onSelect={(date) => {
-                                        setDate(date)
-                                        setOpen(false)
-                                    }}
-                                />
-                            </PopoverContent>
-                        </Popover>
-                    </div>
-                    <UserDropdown />
+
+                    {/* <UserDropdown /> */}
                     <ModeToggle />
                 </div>
             </div>
