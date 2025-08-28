bbb6647231b3e1159dc08e94803b6030756e59d3
KinzixInfotech <mansajami2020@gmail.com>
2025-08-28 23:05:24 +0530

    updated

--- src/app/login/LoginPh.jsx
+++ src/app/login/LoginPh.jsx
@@ -15,6 +15,7 @@ import animation from "../../../public/er.json";
 import { Eye, EyeOff } from "lucide-react";
 import Lottie from "lottie-react"
 import Link from "next/link"
+import Image from "next/image"
 
 export default function LoginPhoto({ className, ...props }) {
     const router = useRouter()
@@ -115,138 +116,149 @@ export default function LoginPhoto({ className, ...props }) {
     }
 
     return (
-        <div className="relative w-full overflow-hidden flex h-screen flex-col items-center justify-center p-6 md:p-10">
-            {loadingl ? (
-                <div>
-                    <Loader2 size={45} className="animate-spin" />
-                </div>
-            ) : (
-                <div className="w-full max-w-sm md:max-w-3xl z-40">
-                    {
-                        mode ? (
-                            <div className="w-full gap-2 h-full flex  flex-col items-center justify-center" >
-                                <Lottie
-                                    animationData={animation}
-                                    className="flex justify-center items-center w-96"
-                                    loop={true}
-                                />
+        <div>
+            <div className="relative w-full overflow-hidden flex h-screen flex-col  p-6 md:p-10">
 
-                                <span className="text-5xl text-center font-bold capitalize">Oops, School Doesn't exist</span>
-                                <span className="text-muted-foreground text-center text-lg capitalize ">
-                                    Please Make Sure The School code is correct
-                                </span>
-                                <Link href="/schoollogin">
-                                    <Button className='mb-3.5 rounded-sm hover:border-black border-2 border-primary transition-all font-bold cursor-pointer hover:bg-muted hover:text-black' size="lg">Back To Login</Button>
-                                </Link>
-                            </div>
-                        ) : (
-                            <div className={cn("flex flex-col gap-6", className)} {...props}>
-                                <Card className="overflow-hidden p-0">
-                                    <CardContent className="grid p-0 md:grid-cols-2">
-                                        {/* {school ? (
+                <div className="flex flex-col items-center justify-center">
+                    <div className="bg-muted w-fit py-1.5 rounded-lg px-5  mb-3  ">
+                        <Image src='/edu.png' width={200} height={200} alt="EduBreezy" priority />
+                    </div>
+                    {loadingl ? (
+                        <div>
+                            <Loader2 size={45} className="animate-spin" />
+                        </div>
+                    ) : (
+                        <div className="w-full max-w-sm md:max-w-3xl z-40">
+                            {
+                                mode ? (
+                                    <div className="w-full gap-2 h-full flex  flex-col items-center justify-center" >
+                                        <Lottie
+                                            animationData={animation}
+                                            className="flex justify-center items-center w-96"
+                                            loop={true}
+                                        />
+
+                                        <span className="text-5xl text-center font-bold capitalize">Oops, School Doesn't exist</span>
+                                        <span className="text-muted-foreground text-center text-lg capitalize ">
+                                            Please Make Sure The School code is correct
+                                        </span>
+                                        <Link href="/schoollogin">
+                                            <Button className='mb-3.5 rounded-sm hover:border-black border-2 border-primary transition-all font-bold cursor-pointer  hover:bg-muted hover:text-black' size="lg">Back To Login</Button>
+                                        </Link>
+                                    </div>
+                                ) : (
+                                    <div className={cn("flex flex-col gap-6", className)} {...props}>
+                                        <Card className="overflow-hidden p-0 border-none">
+                                            <CardContent className="grid p-0 md:grid-cols-2">
+                                                {/* {school ? (
                                 <p>School: {school.name} ({school.schoolCode})</p>
                             ) : schoolCode ? (
                                 <p>Looking for school: {schoolCode}...</p>
                             ) : (
                                 <p>No school code in URL</p>
                             )} */}
-                                        <form className="p-6 md:p-8" onSubmit={handleLogin}>
-                                            <div className="flex flex-col gap-6">
-                                                <div className="flex flex-col items-center text-center">
-                                                    <h1 className="text-2xl font-bold">Welcome</h1>
-                                                    <p className="text-muted-foreground text-balance">
-                                                        Login to your Edubreezy workspace!
-                                                    </p>
-                                                </div>
-                                                <div className="grid gap-3">
-                                                    <Label htmlFor="email">Email</Label>
-                                                    <Input
-                                                        id="email"
-                                                        type="email"
-                                                        placeholder="Enter Your Email"
-                                                        required
-                                                        value={email}
-                                                        onChange={(e) => setEmail(e.target.value)}
-                                                    />
-                                                </div>
-                                                <div className="grid gap-3">
-                                                    <div className="flex items-center">
-                                                        <Label htmlFor="password">Password</Label>
-                                                        <a href="#" className="ml-auto text-sm underline-offset-2 hover:underline">
+                                                <form className="p-6 md:p-8" onSubmit={handleLogin}>
+                                                    <div className="flex flex-col gap-6">
+                                                        <div className="flex flex-col items-center text-center">
+                                                            <h1 className="text-2xl font-bold">Welcome</h1>
+                                                            <p className="text-muted-foreground text-balance">
+                                                                Login to your Edubreezy workspace!
+                                                            </p>
+                                                        </div>
+                                                        <div className="grid gap-3">
+                                                            <Label htmlFor="email">Email</Label>
+                                                            <Input
+                                                                id="email"
+                                                                type="email"
+                                                                placeholder="Enter Your Email"
+                                                                required
+                                                                value={email}
+                                                                className='bg-muted border-none py-6 font-semibold'
+                                                                onChange={(e) => setEmail(e.target.value)}
+                                                            />
+                                                        </div>
+                                                        <div className="grid gap-3">
+                                                            <div className="flex items-center">
+                                                                <Label htmlFor="password">Password</Label>
+                                                                {/* <a href="#" className="ml-auto text-sm underline-offset-2 hover:underline">
                                                             Forgot your password?
-                                                        </a>
+                                                        </a> */}
+                                                            </div>
+                                                            <div className="relative">
+                                                                <Input
+                                                                    id="password"
+                                                                    type={showPassword ? "text" : "password"}
+                                                                    placeholder="Enter Your Password"
+                                                                    required
+                                                                    className='bg-muted border-none py-6 font-semibold'
+                                                                    value={password}
+                                                                    onChange={(e) => setPassword(e.target.value)}
+                                                                />
+                                                                <button
+                                                                    type="button"
+                                                                    onClick={() => setShowPassword(!showPassword)}
+                                                                    className="absolute right-3 cursor-pointer top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                                                                    aria-label={showPassword ? "Hide password" : "Show password"}
+                                                                >
+                                                                    {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                                                                </button>
+
+                                                            </div>
+                                                            <Button
+                                                                type="submit"
+                                                                className="w-full text-white"
+                                                                disabled={loading || alreadyLoggedIn}
+                                                            >
+                                                                {alreadyLoggedIn ? (
+                                                                    "Already Logged In"
+                                                                ) : loading ? (
+                                                                    <span className="flex items-center justify-center gap-2">
+                                                                        <Loader2 className="h-4 w-4 animate-spin" />
+                                                                        Logging in...
+                                                                    </span>
+                                                                ) : (
+                                                                    "Login"
+                                                                )}
+                                                            </Button>
+                                                        </div>
                                                     </div>
-                                                    <div className="relative">
-                                                        <Input
-                                                            id="password"
-                                                            type={showPassword ? "text" : "password"}
-                                                            placeholder="Enter Your Password"
-                                                            required
-                                                            value={password}
-                                                            onChange={(e) => setPassword(e.target.value)}
+                                                </form>
+
+                                                <div className="bg-muted relative hidden md:block">
+                                                    {school ? (
+                                                        <img
+                                                            src={school.profilePicture}
+                                                            alt="Image"
+                                                            className="absolute pointer-events-none inset-0 h-full w-full object-fit"
                                                         />
-                                                        <button
-                                                            type="button"
-                                                            onClick={() => setShowPassword(!showPassword)}
-                                                            className="absolute right-3 cursor-pointer top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
-                                                            aria-label={showPassword ? "Hide password" : "Show password"}
-                                                        >
-                                                            {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-                                                        </button>
+                                                    ) : schoolCode ? (
+                                                        <p></p>
+                                                    ) : (
+                                                        <img
+                                                            src={image.src}
+                                                            alt="Image"
+                                                            className="absolute pointer-events-none inset-0 h-full w-full object-cover"
+                                                        />
+                                                    )}
 
-                                                    </div>
-                                                    <Button
-                                                        type="submit"
-                                                        className="w-full text-white"
-                                                        disabled={loading || alreadyLoggedIn}
-                                                    >
-                                                        {alreadyLoggedIn ? (
-                                                            "Already Logged In"
-                                                        ) : loading ? (
-                                                            <span className="flex items-center justify-center gap-2">
-                                                                <Loader2 className="h-4 w-4 animate-spin" />
-                                                                Logging in...
-                                                            </span>
-                                                        ) : (
-                                                            "Login"
-                                                        )}
-                                                    </Button>
                                                 </div>
-                                            </div>
-                                        </form>
+                                            </CardContent>
+                                        </Card>
 
-                                        <div className="bg-muted relative hidden md:block">
-                                            {school ? (
-                                                <img
-                                                    src={school.profilePicture}
-                                                    alt="Image"
-                                                    className="absolute pointer-events-none inset-0 h-full w-full object-cover"
-                                                />
-                                            ) : schoolCode ? (
-                                                <p></p>
-                                            ) : (
-                                                <img
-                                                    src={image.src}
-                                                    alt="Image"
-                                                    className="absolute pointer-events-none inset-0 h-full w-full object-cover"
-                                                />
-                                            )}
-
+                                        <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+                                            By clicking Login, you agree to our <a href="#">Terms of Service</a> and{" "}
+                                            <a href="#">Privacy Policy</a>.
                                         </div>
-                                    </CardContent>
-                                </Card>
+                                    </div>
+                                )
+                            }
 
-                                <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-                                    By clicking continue, you agree to our <a href="#">Terms of Service</a> and{" "}
-                                    <a href="#">Privacy Policy</a>.
-                                </div>
-                            </div>
-                        )
-                    }
+                        </div >
+                    )}
+                </div>
 
-                </div >
-            )}
+            </div >
 
-        </div >
+        </div>
     )
 }
